0ab8f4022a0f0df5ce99592f8778811bd354a227
rccblt <contact@rccblt.com>
2015-06-12 17:28:07 -0700

    v4

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,4 @@
+
 <?php
 // Check for empty fields
 if(empty($_POST['name'])  		||
@@ -9,20 +10,18 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
+	
 $name = $_POST['name'];
-$email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-if ($email === FALSE) {
-    echo 'Invalid email';
-    exit(1);
-}
+$email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+	
 // Create the email and send the message
 $to = 'caleblinden@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: caleblinden@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$headers = "From: caleblinden@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;
+return true;			
 ?>
